aad5aa7e9e4c343877b5834c51c1c4da0abd9d34
Chinedu Dara <darachinedu@gmail.com>
2019-03-02 17:08:49 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'));
+app.use((req, res, next) => {
+fs.appendFile('server.log', `${new Date().toString()}: ${req.method} ${req.url}\n`, (error) => {
+  if (error) {console.log(error, 'Unable to write request to log file.')};
+});
+console.log(`${new Date().toString()}: ${req.method} ${req.url}`);
+next();
+});
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+})
+
+hbs.registerPartials('views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  // res.send('Hello from Express!');
+  res.render('home.hbs', {
+    title: 'Home',
+    message: 'Welcome message'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    title: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Application started on port 3000');
+});
